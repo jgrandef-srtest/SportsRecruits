a8e2364afcdbb66661522c174ff51f1a02546534
John Grandefeld <jgrandef+sr@gmail.com>
2021-07-26 13:59:42 -0400

    just some housekeeping

--- app/Rankings.php
+++ app/Rankings.php
@@ -2,9 +2,12 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class Rankings extends Model
 {
-    //
+    public function user() {
+	return $this->belongsTo(User::class);
+    }
 }
