bae4179364beeb019be56688eb66a2cfa0904841
John Grandefeld <jgrandef+sr@gmail.com>
2021-07-26 13:51:19 -0400

    added command logic for normalizeRank

--- app/Console/Commands/NormalizeRank.php
+++ app/Console/Commands/NormalizeRank.php
@@ -4,6 +4,13 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
+use App\User;
+use App\Rankings;
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+
 class NormalizeRank extends Command
 {
     /**
@@ -11,14 +18,14 @@ class NormalizeRank extends Command
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    protected $signature = 'normalize:rank';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Moves ranks from Users model to Rankings model ';
 
     /**
      * Create a new command instance.
@@ -35,8 +42,28 @@ class NormalizeRank extends Command
      *
      * @return int
      */
-    public function handle()
+    public function handle(User $user)
     {
-        return 0;
+	// in theory we should probably do some better Exception handling / formatting but that requires 
+	// meddling around with the existing report() / render() methods of the exception handler in the base install
+		
+	//Move ranking data from users.ranking to rankigns.ranking
+	$users = $user::all();
+	foreach ($users as $u) {
+		$r = new Rankings();
+		$r->user_id = $u->id;
+		$r->ranking = $u->ranking;
+		$r->save();
+	}
+
+	//Drop rankigns table
+        if (Schema::hasColumn('users', 'ranking')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->dropColumn('ranking');
+            });
+        }
+
+	echo "Great Success!\n" . count($users) . " user rankings added to rankings table.\nColumn users.ranking dropped.\n";
+		
     }
 }
